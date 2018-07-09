6d84aece2a3df5f7ce6b1eeb27df43b032133e82
CoderLobby <coderlobby@gmail.com>
2018-07-09 15:27:38 +1000

    Changed Library

--- LibraryMaintainance/Library.cs
+++ LibraryMaintainance/Library.cs
@@ -8,6 +8,30 @@ namespace LibraryMaintainance
 {
 	class Library
 	{
-				
+		
+		public List<Shelf> Shelves { get; set; }
+
+		Library(List<Shelf> shelves)
+		{
+			Shelves = shelves;
+		}
+
+		public List<Student> Students { get; set; }
+	}
+
+	class Shelf
+	{
+		// Naming Convension: CoderLobby
+		string Topic { get; set; }
+		List<Book> Books { get; set; }
+
+		Shelf(string topic, List<Book> books)
+		{
+			Topic = topic;
+			var temp = from book in books
+					   where book.Topic.ToLower() == Topic.ToLower()
+					   select book;
+			Books = temp.ToList();
+		}
 	}
 }
